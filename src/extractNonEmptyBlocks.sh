99f01946c0f25ff776d410ca43751312a857201d
Julius Rahaus <82526382+Jaysolutely@users.noreply.github.com>
2021-09-20 17:42:15 +0200

    Update extractNonEmptyBlocks.sh

--- src/extractNonEmptyBlocks.sh
+++ src/extractNonEmptyBlocks.sh
@@ -3,7 +3,7 @@
 tail -n0 -f $1 | \
 while read line
 do
-	result=( $(echo $line | awk 'BEGIN {FS="#|,| |\t"}; /produce_block/ {print $11, $18}') )
+	result=( $(echo $line | awk 'BEGIN {FS="#|,| |\t"}; /trxs/ {print $11, $18}') )
 	if [ ${#result[@]} -eq 2 ] && [ ${result[1]} -gt 0 ]
 	then
 		echo "${result[0]}"
